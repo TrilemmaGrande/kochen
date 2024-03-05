f9f2325ce71979330a277a4e1b7aa7a8893ae0a1
Nils Reinshagen <nils-reinshagen@protonmail.com>
2024-03-05 19:16:58 +0100

    spacing

--- resources/views/recipes/create.blade.php
+++ resources/views/recipes/create.blade.php
@@ -35,13 +35,12 @@
     <p>Feld erforderlich!</p>
     @enderror
 
-
     <br>
     <br>
     <br>
 
     <label for="preparation">Zubereitung</label>
-    <textarea type="text" name="preparation" id="preparationEditor" value="{{old('preparation')}}"placeholder="z.B. Alles mischen und Braten"></textarea>
+    <textarea type="text" name="preparation" id="preparationEditor" value="{{old('preparation')}}"placeholder="z.B. Alles mischen und Braten">{{old('preparation')}}</textarea>
     @error('preparation')
     <p>Feld erforderlich!</p>
     @enderror
